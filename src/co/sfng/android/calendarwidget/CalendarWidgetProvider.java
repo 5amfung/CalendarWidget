95c1424f247da05cef18367d5a43ac64de6d5448
Sam Fung <sam.fung@gmail.com>
2013-09-13 00:53:21 -0700

    Render a simple nav bar and create some layouts for different parts of the calendar.

--- src/co/sfng/android/calendarwidget/CalendarWidgetProvider.java
+++ src/co/sfng/android/calendarwidget/CalendarWidgetProvider.java
@@ -1,6 +1,6 @@
 package co.sfng.android.calendarwidget;
 
-import java.util.logging.Logger;
+import java.util.Calendar;
 
 import android.annotation.TargetApi;
 import android.appwidget.AppWidgetManager;
@@ -8,29 +8,57 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.RemoteViews;
 
 
 public class CalendarWidgetProvider extends AppWidgetProvider {
 
-	private final static Logger log = Logger.getLogger(CalendarWidgetProvider.class.toString());
-	
-	@Override
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-	public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId,
-			Bundle newOptions) {
-		super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId,	newOptions);
-		log.info("onAppWidgetOptionsChanges()");
-	}
+	private static final String LOG_TAG = CalendarWidgetProvider.class.toString();
 
 	@Override
-	public void onEnabled(Context context) {
-		super.onEnabled(context);
-		log.info("onEnabled");
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+			int appWidgetId, Bundle newOptions) {
+		Log.i(LOG_TAG, "onAppWidgetOptionsChanges()");
+		super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId,	newOptions);
+
+		Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+		int minWidthInDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+		int maxWidthInDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+
+		int minHeightInDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+		int maxHeightInDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+
+		Log.i(LOG_TAG, "minWidthInDp = " + minWidthInDp);
+		Log.i(LOG_TAG, "maxWidthInDp = " + maxWidthInDp);
+
+		Log.i(LOG_TAG, "minHeightInDp = " + minHeightInDp);
+		Log.i(LOG_TAG, "maxHeightInDp = " + maxHeightInDp);
+
+		render(context, appWidgetId);
 	}
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		Log.i(LOG_TAG, "onUpdate()");
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		log.info("onUpdate()");
+
+		for (int appWidgetId: appWidgetIds) {
+			Log.i(LOG_TAG, "app widget id = " + appWidgetId);
+			render(context, appWidgetId);
+		}
+	}
+
+	private void render(Context context, int appWidgetId) {
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+		RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+		Calendar cal = Calendar.getInstance();
+		view.setTextViewText(R.id.month_year_label, DateFormat.format("MMM yyyy", cal));
+		view.removeAllViews(R.id.calendar);
+
+		appWidgetManager.updateAppWidget(appWidgetId, view);
 	}
 }
